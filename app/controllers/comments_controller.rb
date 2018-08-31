7a900b08143163bc41d0120a60c19e39ff820986
User <user@yuzanoMacBook-ea.local>
2018-08-31 17:47:03 +0900

    Initial commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,12 @@
+class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.create(text: comment_params[:text], tweet_id: comment_params[:tweet_id], user_id: current_user.id)
+    redirect_to "/tweets/#{@comment.tweet.id}"
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(tweet_id: params[:tweet_id])
+  end
+end
